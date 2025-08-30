c5dba605287f8263b26d6af26640f09bd8c87a08
DHRUV SARVAIYA <dvs@DHRUVs-MacBook-Air.local>
2025-08-30 16:45:21 +0530

    signup

--- app/signup/page.js
+++ app/signup/page.js
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default function Signup() {
